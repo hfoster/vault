a27245d2ea3824997160d24529062a9556002dc3
Hartnell Foster <hartnell.foster@googlemail.com>
2018-07-28 17:48:02 +0200

    Security Groups and Load Balancers configuration

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -9,10 +9,11 @@ data "aws_ami" "centos7" {
 }
 
 resource "aws_launch_configuration" "app_conf" {
-  name          = "app-config"
-  image_id      = "${data.aws_ami.centos7.id}"
-  instance_type = "t2.micro"
-  key_name      = "${var.ec2_key_pair}"
+  name            = "app-config"
+  image_id        = "${data.aws_ami.centos7.id}"
+  instance_type   = "t2.micro"
+  key_name        = "${var.ec2_key_pair}"
+  security_groups = ["${aws_security_group.web.id}", "${aws_security_group.ssh_in.id}"]
 
   lifecycle {
     create_before_destroy = true
@@ -25,13 +26,16 @@ resource "aws_autoscaling_group" "app_asg" {
   min_size             = 1
   max_size             = 3
   desired_capacity     = 1
+  vpc_zone_identifier  = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}", "${aws_subnet.private3.id}"]
+  load_balancers       = ["${aws_elb.app_lb.id}"]
 }
 
 resource "aws_launch_configuration" "web_conf" {
-  name          = "web-config"
-  image_id      = "${data.aws_ami.centos7.id}"
-  instance_type = "t2.nano"
-  key_name      = "${var.ec2_key_pair}"
+  name            = "web-config"
+  image_id        = "${data.aws_ami.centos7.id}"
+  instance_type   = "t2.nano"
+  key_name        = "${var.ec2_key_pair}"
+  security_groups = ["${aws_security_group.web.id}", "${aws_security_group.ssh_jump.id}"]
 
   lifecycle {
     create_before_destroy = true
@@ -44,4 +48,6 @@ resource "aws_autoscaling_group" "web_asg" {
   min_size             = 1
   max_size             = 3
   desired_capacity     = 1
+  vpc_zone_identifier  = ["${aws_subnet.public1.id}", "${aws_subnet.public2.id}", "${aws_subnet.public3.id}"]
+  load_balancers       = ["${aws_elb.web_lb.id}"]
 }
--- terraform/elb.tf
+++ terraform/elb.tf
@@ -1,2 +1,54 @@
 #Elastic Load Balencers
+resource "aws_elb" "web_lb" {
+  name                      = "web-load-balancer"
+  cross_zone_load_balancing = true
+  subnets                   = ["${aws_subnet.public1.id}", "${aws_subnet.public2.id}", "${aws_subnet.public3.id}"]
+  security_groups           = ["${aws_security_group.web.id}"]
+  internal                  = false
 
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  tags {
+    Name = "web-elb"
+  }
+}
+
+resource "aws_elb" "app_lb" {
+  name                      = "app-load-balancer"
+  cross_zone_load_balancing = true
+  subnets                   = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}", "${aws_subnet.private3.id}"]
+  security_groups           = ["${aws_security_group.web.id}"]
+  internal                  = true
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  tags {
+    Name = "app-elb"
+  }
+}
