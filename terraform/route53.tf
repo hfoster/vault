1b3503d0073135662ffac8b828cebe6bd2bb4728
Hartnell Foster <hartnell.foster@googlemail.com>
2018-09-15 19:10:02 +0200

    Removed the tools zone

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,4 @@
 #Route53 domain and DNS configuration
-resource "aws_route53_zone" "tools" {
-  name   = "tools.${var.domain_name}"
-  vpc_id = "${data.terraform_remote_state.infrastructure.main_vpc_id}"
-}
-
 resource "aws_route53_record" "vault-external" {
   zone_id = "${var.route53_zone_id}"
   name    = "vault.${var.env_name}.${var.domain_name}"
